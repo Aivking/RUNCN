e305614ff4615e6b1e58fd8b74e351a9ecc26145
Andrei Andreev <razenpok@gmail.com>
2026-03-12 23:41:51 +0100

    XIT CONTC: Add missing condition descriptions

--- src/features/XIT/CONTC/ConditionText.vue
+++ src/features/XIT/CONTC/ConditionText.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { fixed02 } from '@src/utils/format';
 import AddressLink from '@src/features/XIT/CONTC/AddressLink.vue';
+import PrunLink from '@src/components/PrunLink.vue';
 
 defineProps<{ condition: PrunApi.ContractCondition }>();
 </script>
@@ -49,12 +50,43 @@ defineProps<{ condition: PrunApi.ContractCondition }>();
   <template v-else-if="condition.type === 'BASE_CONSTRUCTION'">建造基地</template>
   <template v-else-if="condition.type === 'FINISH_FLIGHT'">完成飞行</template>
   <template v-else-if="condition.type === 'PLACE_ORDER'">下单</template>
-  <template v-else-if="condition.type === 'PRODUCTION_ORDER_COMPLETED'"> 完成生产订单 </template>
+  <template v-else-if="condition.type === 'PRODUCTION_ORDER_COMPLETED'">
+    完成生产订单
+  </template>
   <template v-else-if="condition.type === 'PRODUCTION_RUN'">运行生产</template>
   <template v-else-if="condition.type === 'START_FLIGHT'">开始飞行</template>
   <template v-else-if="condition.type === 'POWER'">成为总督</template>
   <template v-else-if="condition.type === 'REPAIR_SHIP'">修复飞船</template>
   <template v-else-if="condition.type === 'CONTRIBUTION'">贡献</template>
+  <template v-else-if="condition.type === 'CONSTRUCT_SHIP'">建造飞船</template>
+  <template v-else-if="condition.type === 'GATEWAY_FUEL'">
+    加油
+    <PrunLink inline :command="`GTW ${condition.gatewayId?.naturalId}`">{{
+      condition.gatewayId?.name
+    }}</PrunLink>
+  </template>
+  <template v-else-if="condition.type === 'INFRASTRUCTURE_CONSTRUCTION_FINISH'">
+    完成基础设施建设
+  </template>
+  <template v-else-if="condition.type === 'INFRASTRUCTURE_CONSTRUCTION_START'">
+    开始基础设施建设
+  </template>
+  <template v-else-if="condition.type === 'INFRASTRUCTURE_UPGRADE_FINISH'">
+    完成基础设施升级
+  </template>
+  <template v-else-if="condition.type === 'INFRASTRUCTURE_UPGRADE_START'">
+    开始基础设施升级
+  </template>
+  <template v-else-if="condition.type === 'INFRASTRUCTURE_UPKEEP'">
+    维护保养
+    <PrunLink inline :command="`INFU ${condition.infrastructureId?.naturalId}`">{{
+      condition.infrastructureId?.name
+    }}</PrunLink>
+  </template>
+  <template v-else-if="condition.type === 'PICKUP'">提取货物</template>
+  <template v-else-if="condition.type === 'WORKFORCE_PROGRAM_PAYMENT'">
+    支付 {{ fixed02(condition.amount!.amount) }} {{ condition.amount!.currency }}
+  </template>
   <template v-else>
     {{ condition.type }}
   </template>
